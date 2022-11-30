309690d97ea3db153051aa3b2a8293202b2b2518
Michael <Mike@smallie.local>
2022-11-30 19:35:17 +0300

    upload and posts

--- Server/server.go
+++ Server/server.go
@@ -1,54 +1,90 @@
 package main
 
 import (
-	"net/http"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
+
+	"github.com/rs/cors"
 )
 
 func uploadFile(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Uploading File\n")
+	fmt.Fprintf(w, "Uploading File\n")
+	err := r.ParseMultipartForm(10 << 20)
+	if err != nil {
+		fmt.Fprintln(w, "File too big")
+		return
+	}
+
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		fmt.Fprintln(w, "Error receiving file form-data", err)
+		return
+	}
+
+	defer file.Close()
+
+	fmt.Printf("Upload File Name: %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+	tempFile, err := ioutil.TempFile("/Users/mike/fun/fuzzy-wuzzy/Server/temp", "upload-*.jpg")
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+
+	}
+	defer tempFile.Close()
+
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Fprintln(w, "Failed to read file", err)
+		return
+	}
+
+	_, err = tempFile.Write(fileBytes)
+	if err != nil {
+		fmt.Fprintln(w, "Failed to write data to file", err)
+		return
+	}
+
+	fmt.Fprintf(w, "File Uplooaded Successfully File\n")
+
+	w.Header().Set("Location", "http://www.example.org/login")
+	w.WriteHeader(200)
 }
 
-r.ParseMultipartForm(10 << 20)
+func FetchPosts(w http.ResponseWriter, r *http.Request) {
+	files, err := ioutil.ReadDir("./temp")
+	if err != nil {
+		fmt.Fprintln(w, "Failed to fetch posts", err)
+		return
+	}
 
-file, handler, err := r.FormFile("myFile")
-if err != nil {
-	fmt.Println("Error receiving file form-data")
-	fmt.Println(err)
-	return
-
+	var posts = make([]string, len(files))
+	for i, f := range files {
+		posts[i] = f.Name()
+	}
+	body, err := json.Marshal(posts)
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(body)
 }
 
-defer file.Close()
-fmt.Printf("Upload File: %+v\n", handler.Filename)
-fmt.Printf("File Size: %+v\n", handler.Size)
-fmt.Printf("MIME Header: %+v\n", handler.Header)
-
-tempFile, err := ioutil.TempFile("temp-img",
-*upload-*.png)
-if err != nil {
-	fmt.Println(err)
-	return
-
-}
-defer tempFile.Close()
-
-fileBytes, err := ioutil.ReadAll(file)
-if err != nil{
-	fmt.Println(err)
-}
-
-tempFile.Write(fileBytes)
-
-fmt.Fprintf(w, "File successfully uploaded\n")
-
-
-func setupRoutes(){
-	http.HandleFunc("/upload", uploadFile)
-	http.ListenAndServe(":8080", nil)
+func setupRoutes() {
+	mux := http.NewServeMux()
+	port := ":8080"
+	mux.HandleFunc("/upload", uploadFile)
+	mux.HandleFunc("/posts", FetchPosts)
+	handler := cors.Default().Handler(mux)
+	fmt.Printf("I'm running at http://localhost%s\n", port)
+	http.ListenAndServe(port, handler)
 }
 
 func main() {
-	fmt.Println("hello world")
+	setupRoutes()
 }
